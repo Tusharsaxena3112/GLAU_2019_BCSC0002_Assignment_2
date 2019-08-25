6348e1ebceb9c9c55c7ac882b12722a9429a4fa2
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-25 15:58:57 +0530

    commiting ques6

--- section1/question4/src/Task.java
+++ section1/question4/src/Task.java
@@ -1,5 +1,20 @@
+import org.graalvm.compiler.nodes.calc.IntegerTestNode;
+
 public class Task {
 	public String convertIntegerToBinaryString(int decimalNumber) {
-		return "";
+		int rem=0;
+		int bin=0;
+		while (decimalNumber>0){
+			rem=decimalNumber%2;
+			bin=bin*10+rem;
+		}
+		rem=0;
+		int bin1=0;
+		while(bin>0){
+			rem=bin%10;
+			bin1=bin1*10+rem;
+		}
+		String bin2=String.valueOf(bin1);
+		return bin2;
 	}
 }
--- section1/question7/src/Task.java
+++ section1/question7/src/Task.java
@@ -1,3 +1,6 @@
 public class Task {
 	// TODO write the method here and delete this line afterwards!
+    public double evaluateExpression(){
+        return 0.0;
+    }
 }
